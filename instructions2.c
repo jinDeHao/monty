68d52a90f7a71317b5876446bbe0399827861d41
omarIdhmaid <o.idhmaid@gmail.com>
2023-06-22 18:00:28 +0100

    use printf instead of putchar

--- instructions2.c
+++ instructions2.c
@@ -23,7 +23,7 @@ void pchar(stack_t **stack, unsigned int line_number)
 {
 	if (!(*stack))
 		error_pchar_empty(stack, line_number);
-	if ((*stack)->n > 255 || (*stack)->n < 0)
+	if ((*stack)->n > 255 || (*stack)->n < -128)
 		error_not_char(stack, line_number);
 	printf("%c\n", (*stack)->n);
 }
