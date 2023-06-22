8f73f5b4c6d3314b0ce935f6bd3519eddc5cc4b4
soufiane-oumalek <soufian.oumalek@gmail.com>
2023-06-22 16:15:17 +0000

    last update

--- bytecodes/00.m
+++ bytecodes/00.m
@@ -4,3 +4,11 @@ push 10
 push 3
 sub
 pall
+sub
+                                                   push                           7                           push to the stack
+push                       5
+pop
+pint
+#include
+                                                                pop                                                10n
+                                                        pall
